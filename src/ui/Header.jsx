75cdb3ae3bab6cc04b4891f7dc272df4364ee72d
Mathiasheartman <mathiasokpe86@gmail.com>
2025-05-13 12:19:15 +0100

    Initial commit

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -11,7 +11,7 @@ function Header({ homeRef, scrollTo, aboutUsRef, recipeRef, galleryRef }) {
 
   return (
     <header className="fixed top-0 right-0 left-0 z-50 m-auto bg-white px-7 py-2 lg:max-w-[1440px]">
-      <div className="flex items-start justify-between pt-1">
+      <div className="flex items-center justify-between pt-1">
         <div>
           <Logo />
         </div>
@@ -46,8 +46,12 @@ function Header({ homeRef, scrollTo, aboutUsRef, recipeRef, galleryRef }) {
           </ul>
         </nav>
         <p className="top-18 right-10 flex cursor-pointer items-center text-xl">
-          <img className="h-4 md:h-8" src="/call.png" alt="call" />{" "}
-          <span className="text-[0.5rem] text-black hover:text-amber-700 md:text-sm lg:text-lg">
+          <img
+            className="hidden h-4 sm:block md:h-8"
+            src="/call.png"
+            alt="call"
+          />{" "}
+          <span className="hidden text-xs text-black hover:text-amber-700 sm:block md:text-sm lg:text-lg">
             +1(909) 235-9814
           </span>
         </p>
@@ -64,13 +68,13 @@ function Header({ homeRef, scrollTo, aboutUsRef, recipeRef, galleryRef }) {
           className={`absolute right-4 flex flex-col gap-1 sm:hidden`}
         >
           <p
-            className={` ${show ? "menuFirst" : ""} h-0.5 w-5 rounded-full bg-black`}
+            className={` ${show ? "menuFirst" : ""} h-0.5 w-8 rounded-full bg-black`}
           ></p>
           <p
-            className={`${show ? "menuMid" : ""} h-0.5 w-5 rounded-full bg-black`}
+            className={`${show ? "menuMid" : ""} h-0.5 w-8 rounded-full bg-black`}
           ></p>
           <p
-            className={`${show ? "menuLast" : ""} h-0.5 w-5 rounded-full bg-black`}
+            className={`${show ? "menuLast" : ""} h-0.5 w-8 rounded-full bg-black`}
           ></p>
         </button>
       </div>
--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -3,7 +3,7 @@ function Logo() {
     <div className="h-6 md:h-12">
       <a className="flex items-center gap-2" href="">
         <img className="h-5 md:h-10" src="/logo.svg" alt="" />
-        <p className="text-xs text-black md:text-lg">Chef's Kitchen</p>
+        <p className="text-lg text-black">Chef's Kitchen</p>
       </a>
     </div>
   );
