05658e8d555c530a23d105e34ae4367075d6711e
Mathiasheartman <mathiasokpe86@gmail.com>
2025-05-13 11:56:13 +0100

    Initial commit

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -2,7 +2,7 @@ function Logo() {
   return (
     <div className="h-6 md:h-12">
       <a className="flex items-center gap-2" href="">
-        <img className="h-5 md:h-10" src="/src/assets/logo.svg" alt="" />
+        <img className="h-5 md:h-10" src="/logo.svg" alt="" />
         <p className="text-xs text-black md:text-lg">Chef's Kitchen</p>
       </a>
     </div>
